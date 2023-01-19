6e468bae49c816a5eac9c36ef095d696d555aa15
Jordan Bursik <jsbursik@gmail.com>
2023-01-18 21:50:30 -0600

    Tightening up the styling

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -9,6 +9,15 @@ export default function TodoForm() {
       <div class={styles.button} onClick={() => setFormVis((prev) => !prev)}>
         +
       </div>
+      <div class="container">
+        <form>
+          <label for="task-title">Title</label>
+          <input type="text" id="task-title" placeholder="Title goes here..." />
+          <br />
+          <label for="task-content">Content</label>
+          <textarea id="task-content" placeholder="Content goes here..." />
+        </form>
+      </div>
     </>
   );
 }
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -13,12 +13,21 @@ const updateTodo = server$((id) => {
   toggleTodo(id);
 });
 
+export function truncText(text: string): string {
+  let limit = 15;
+  let split = text.split(" ");
+
+  return split.length > limit + 1
+    ? split.slice(0, limit).join(" ") + "..."
+    : text;
+}
+
 export default function Todo({ todo }: { todo: Todo }) {
   return (
-    <div class={styles.container}>
+    <div class="container">
       <div class={styles.tasktext}>
-        <b class={styles.taskname}>{todo.title}</b>
-        <p class={styles.taskcontent}>{todo.content}</p>
+        <b class={styles.taskname}>{truncText(todo.title)}</b>
+        <p class={styles.taskcontent}>{truncText(todo.content)}</p>
       </div>
       <input
         class={styles.completed}
